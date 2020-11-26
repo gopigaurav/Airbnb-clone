583d2af809590ab420f6952785ac65b8f60a3fca
unknown <gopigaurav9@gmail.com>
2020-11-26 09:04:57 +0530

    Updates

---
+++ precache-manifest.aee4a5983d2c1a64db9d477641e85222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ccabec2adb05f529ff35144520f345",
+    "url": "/Airbnb-clone/index.html"
+  },
+  {
+    "revision": "6bb9bb64d1023e6345ff",
+    "url": "/Airbnb-clone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "38def0241561f33f8aeb",
+    "url": "/Airbnb-clone/static/css/main.eca950ed.chunk.css"
+  },
+  {
+    "revision": "6bb9bb64d1023e6345ff",
+    "url": "/Airbnb-clone/static/js/2.1610be5a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Airbnb-clone/static/js/2.1610be5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38def0241561f33f8aeb",
+    "url": "/Airbnb-clone/static/js/main.52499b9f.chunk.js"
+  },
+  {
+    "revision": "2444f55e81115dd66e26",
+    "url": "/Airbnb-clone/static/js/runtime-main.fc129058.js"
+  }
+]);
