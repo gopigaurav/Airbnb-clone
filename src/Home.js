7906af2a746939e1bf211bd5b404256942f1457a
unknown <gopigaurav9@gmail.com>
2020-11-26 08:59:52 +0530

    airbnb-clone

---
+++ src/Home.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './Home.css';
+import Banner from './Banner'
+import Card from './Card'
+
+// ES7 snippets to do 'rfce'
+
+function Home() {
+    return (
+        <div className='home'>
+            <Banner />
+
+            <div className='home__section'>
+            <Card
+                src="https://pix10.agoda.net/hotelImages/6395607/-1/9c841444ec7a198e3fc2da32077ea95b.jpg?s=1024x768"
+                title="Online Experiences"
+                description="Unique activities we can do together, led by a world of hosts."
+            />
+            <Card
+                src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+                title="Unique stays"
+                description="Spaces that are more than just a place to sleep."
+            />
+            <Card
+                src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+                title="Entire homes"
+                description="Comfortable private places, with room for friends or family."
+            />
+            </div>
+            <div className='home__section'>
+            <Card
+                src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
+                title="3 Bedroom Flat in Bournemouth"
+                description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
+                price="₹2130/night"
+            />
+            <Card
+                src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+                title="Penthouse in London"
+                description="Enjoy the amazing sights of London with this stunning penthouse"
+                price="₹3250/night"
+            />
+             <Card
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzSjNDXHVj5rL0dq2ch3qAMgu5IRc3T8Kurw&usqp=CAU"
+                title="Penthouse in London"
+                description="Enjoy the amazing sights of London with this stunning penthouse"
+                price="₹3520/night"
+            />
+            <Card
+                src="https://media.nomadicmatt.com/2018/apartment.jpg"
+                title="1 Bedroom apartment"
+                description="Superhost with great amenities and a fabolous shopping complex nearby"
+                price="₹7059/night"
+            />
+            </div>
+            <div className='home__section'>
+            <Card
+                src="https://dammedia.ledvance.info/media/img/asset-4086347//c,x,0,y,1058,w,4165,h,2342/s,x,1600,y,0/iStock-185270129.jpg"
+                title="Penthouse in London"
+                description="Enjoy the amazing sights of London with this stunning penthouse"
+                price="₹3599/night"
+            />
+             <Card
+                src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+                title="Penthouse in London"
+                description="Enjoy the amazing sights of London with this stunning penthouse"
+                price="₹1290/night"
+            />
+            <Card
+                src="https://media.nomadicmatt.com/2018/apartment.jpg"
+                title="1 Bedroom apartment"
+                description="Superhost with great amenities and a fabolous shopping complex nearby"
+                price="₹3900/night"
+            />  
+            </div>
+        </div>
+    )
+}
+
+export default Home
